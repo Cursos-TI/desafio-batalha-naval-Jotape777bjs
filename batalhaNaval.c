1bc76af833f49a0b9e4ed7c1c8d901e3929fa957
Jotape777bjs <joaopedrosantanadebarros4@gmail.com>
2025-09-23 19:38:01 +0000

    modifiquei as condicionais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,6 +5,10 @@ int main() {
     char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};//vetor para as letras que indica as coluna
     int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};// vetor para a linha lateral de numeros que indica as linhas
     int tabuleiro[10][10] = {0};//matriz do tabuleiro
+    int cone1[5][5];
+    int alturanavio1, alturanavio2;
+    int imp_tabuleiro, imp_tabuleiro2;
+    int modificacao;
 
     for (int imp_coluna = 0; imp_coluna < 10; imp_coluna++)//repetir as letras da coluna
     {
@@ -14,36 +18,31 @@ int main() {
                 printf("\n");
             }
     }
-    for (int imp_tabuleiro = 0; imp_tabuleiro < 10; imp_tabuleiro++){//repetição do primeiro indice do tabuleiro
+    for (imp_tabuleiro = 0; imp_tabuleiro < 10; imp_tabuleiro++){//repetição do primeiro indice do tabuleiro
         printf("%d", linha[imp_tabuleiro]);//numerar as linhas do tabuleiro
-        for (int imp_tabuleiro2 = 0; imp_tabuleiro2 < 10; imp_tabuleiro2++){//repetição do segundo indice do tabuleiro
-            printf(" %d ", tabuleiro[imp_tabuleiro][imp_tabuleiro2]);
-            //navio horizontal
-            tabuleiro[4][5] = 3;
-            tabuleiro[4][6] = 3;
-            tabuleiro[4][7] = 3;
-            //navio na vertical
-            tabuleiro[2][2] = 3;
-            tabuleiro[3][2] = 3;
-            tabuleiro[4][2] = 3;
-            //navio diagonal 1
-            tabuleiro[0][5] = 3;
-            tabuleiro[1][4] = 3;
-            tabuleiro[2][3] = 3;
-            //navio diagonal 2
-            tabuleiro[6][4] = 3;
-            tabuleiro[7][3] = 3;
-            tabuleiro[8][2] = 3;
-            if (imp_tabuleiro2 == 9)//para imprimir corretamento o tabuleiro na tela, toda vez que a 9 ele salta para a proxima linha
-            {
-                printf("\n");
+        for (imp_tabuleiro2 = 0; imp_tabuleiro2 < 10; imp_tabuleiro2++){//repetição do segundo indice do tabuleiro
+            if (tabuleiro[4][imp_tabuleiro2] = (imp_tabuleiro2 >= 5 && imp_tabuleiro2 <= 7)){
+                tabuleiro[4][imp_tabuleiro2] = 3;
             }
-             
+            if (tabuleiro[imp_tabuleiro][2] = (imp_tabuleiro >= 1 && imp_tabuleiro <= 3)){
+                tabuleiro[imp_tabuleiro][2] = 3;
+            }
+            if (imp_tabuleiro == imp_tabuleiro2 && imp_tabuleiro >= 7 && imp_tabuleiro2 <= 9){
+                tabuleiro[imp_tabuleiro][imp_tabuleiro2] = 3;
+            }
+            if (imp_tabuleiro + imp_tabuleiro2 == 9 && imp_tabuleiro <= 2 && imp_tabuleiro2 <= 9){
+                tabuleiro[imp_tabuleiro][imp_tabuleiro2] = 3;
+            }
+            
+
+            printf(" %d ", tabuleiro[imp_tabuleiro][imp_tabuleiro2]);   
         }
+
+            printf("\n");
     }
 
-    printf("\n");
-
+    
+   
     
     
     
